e60eb17624a6994f4a994d7dc4457985f0eb7930
sakshigoyal58 <sakshigoyal58@gmail.com>
2026-05-30 21:28:31 +0400

    Deploy to aws

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14607dec8cced0ced3ee56e0ea046136961051e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28f062c34c177ab0c1fd5538c6b517d996b31670")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
